02c731205b0c73a164d395466abea829072988bb
Wasif Zaman <wasifzaman94@gmail.com>
2021-02-02 23:04:51 +0500

    Item Review and Order Cart Apis

---
+++ src/main/java/com/shopping/portal/controller/OrderCartController.java
@@ -0,0 +1,29 @@
+package com.shopping.portal.controller;
+
+import com.shopping.portal.business.base.OrderCartBusiness;
+import com.shopping.portal.request.OrderCartRequest;
+import com.shopping.portal.response.BaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/order")
+public class OrderCartController {
+
+    @Autowired
+    OrderCartBusiness orderCartBusiness;
+
+    @PostMapping("/cart")
+    public ResponseEntity<BaseResponse> saveOrderCart(@Validated @RequestBody OrderCartRequest orderCartRequest) {
+
+        orderCartBusiness.saveOrderCart(orderCartRequest);
+        return ResponseEntity.ok(orderCartBusiness.saveOrderCart(orderCartRequest));
+    }
+}
